9485f20b380b0e7fa85d5edeff85d317dfa64fab
Neo Gao <neogao@earlylight.com.hk>
2018-02-07 11:06:05 +0800

    init spec

---
+++ Example/Pods/Target Support Files/ELTestPod/ELTestPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ELTestPod : NSObject
+@end
+@implementation PodsDummy_ELTestPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-ELTestPod_Example/Pods-ELTestPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ELTestPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ELTestPod_Example
+@end
